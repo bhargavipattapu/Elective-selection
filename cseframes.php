097c4ade852c3ed5f736f01f5ce27814712b0aac
bhargavipattapu <53491392+bhargavipattapu@users.noreply.github.com>
2019-07-31 00:13:50 +0530

    Add files via upload

---
+++ cseframes.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+$_SESSION['username'];
+?>
+
+<html>
+<head><title>frames</title></head>
+<frameset cols="33%,34%,33%">
+<frame name="f1" src="cse3-1.html">
+<frame name="f2" src="cse3-2.html">
+<frame name="f3" src="cse4-1.html">
+</frameset>
+</html>
